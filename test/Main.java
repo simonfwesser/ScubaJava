b25cb3a205cdc6d415ba8a0e94c2a1eec70a44d7
9736109 <9736109@LI-S064-0T22>
2018-09-14 12:17:25 -0400

    commit 2018_09_14 11h02

--- test/Main.java
+++ test/Main.java
@@ -2,27 +2,13 @@
 import dataAccess.CustomerDAO;
 import dataAccess.DAO;
 import dataAccess.DbConnection;
-import entite.Costumer;
+import entite.Customer;
 import java.util.List;
 
 
 public class Main {
     
     public static void main(String[] args){
-    
-        //DataAccess.getInstance().open();
-        DAO costumerDAO = new CustomerDAO(DbConnection.getInstance().getSessionFactory());
-        
-        List<Costumer> costumers = costumerDAO.getAll();
-        
-        costumerDAO.closeSession();
-        
-        for(Costumer costumer : costumers){
-            System.out.println(costumer.getFirstName() +" " +costumer.getLastName());
-        }
-
-        DbConnection.getInstance().close();
-
         
     }
     
